a11f9561413ff12bb7e3adacea19c2e90d06622f
Lucy Qin <lucyqin@dhcp-18-189-74-40.dyn.MIT.EDU>
2017-04-22 18:58:35 -0400

    updated message

--- twilio/app.py
+++ twilio/app.py
@@ -4,9 +4,9 @@ from flask import Flask, render_template, request
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
 
-
+import random
 import atexit
-
+import csv
 
 # App Modules
 import userManager
@@ -24,15 +24,19 @@ TWILIO_MSG_ID = 'MG09846332b0012e9557d19ef6d7ac12ea'
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 
+
 def sendMessageOfDay():
     all_users = userManager.getAllUsers()
-    
+
+
+    msg = getRandomEntry()    
+
     for user in all_users:
         print("Sending message to", user)
         client.messages.create(
                 to="+" + user, 
                 from_=TWILIO_NUM,
-                body="WOOOOHOOOOOOO!"
+                body=msg
             )
 
 
@@ -51,7 +55,7 @@ scheduler.start()
 scheduler.add_job(
     func=sendMessageOfDay,
     # trigger=IntervalTrigger(seconds=86400)
-    trigger=IntervalTrigger(seconds=30),
+    trigger=IntervalTrigger(seconds=2),
     id='printing job',
     name='testing123',
     replace_existing=True)
@@ -66,7 +70,6 @@ userManager.loadData()
 
 @app.route("/")
 def displayIndex():
-#    print("OS", os.environ)
     return render_template('index.html')
 
 
@@ -79,13 +82,45 @@ def sms():
 
     if userManager.isUser(number):
         print("User: {}, Message: {}".format(number, message_body))
-        resp.message("YOU'RE BACK!'")
+        resp.message(getRandomEntry())
     else:
         print("New user: ", number);
         userManager.addUser(number)
         resp.message(INIT_MSG.format(number))
 
     return str(resp)
+
+
+def getRandomEntry():
+    file_path = "../data/StopAndFrisk-person.csv"
+
+    suspicions = ["Reasonable Suspicion", "Probable Cause", "Intel"]
+    actions = ["Stopped", "Observed","Encountered"]
+    cities = ["Dorchester", "Boston", "Roxbury", "Jamaica Plain", "Mattapan", "Brighton"]
+
+    msg = ""
+    with open(file_path, 'rb') as csvfile:
+        rdr = csv.reader(csvfile)
+        row_count = sum(1 for row in rdr)
+        rand_index = random.randrange(row_count)
+        csvfile.seek(0)
+        for i, row in enumerate(rdr):
+            if i == rand_index:
+                date = row[2]
+                age = row[5]
+                sex = row[3].lower()
+                race = row[4].lower()
+    
+                msg = "On " + date + " police " + random.choice(actions).lower() +" a " + age + " year-old " + race + " " + sex + " in " + random.choice(cities) + " citing: " + random.choice(suspicions).lower()
+                msg += "\n\nFind out more at http://stopfrisking.me"
+
+    return msg
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
